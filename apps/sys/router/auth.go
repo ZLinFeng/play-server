7ad75a7cf950188b5056cde5ef34cd4e332ffffc
Zhu <1@qq.com>
2024-10-30 10:22:37 +0800

    login

--- apps/sys/router/auth.go
+++ apps/sys/router/auth.go
@@ -9,4 +9,5 @@ type AuthRouter struct{}
 func (router *AuthRouter) InitAuthRouter(Router *gin.RouterGroup) {
 	authRouter := Router.Group("auth")
 	authRouter.POST("login", authApi.Login)
+	authRouter.POST("register", authApi.Register)
 }
